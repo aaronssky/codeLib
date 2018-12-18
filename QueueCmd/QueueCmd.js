296a66374ab925edb086f0c608554d35e6f8dd2a
chenjunlun <chenjunlun@yaochufa.com>
2018-12-18 19:49:58 +0800

    脚本功能扩展

--- QueueCmd/QueueCmd.js
+++ QueueCmd/QueueCmd.js
@@ -1,3 +1,8 @@
+/**
+ * QueueCmd 一个node脚本执行命令/方法队列
+ * author: Aron Chen
+ */
+
 var process = require('child_process');
 
 console.chunk = (p) => {
@@ -34,44 +39,80 @@ console.blue = (p) => {
     console.log("\033[47;34m " + p + " \033[0m")
 }
 
+
 class QueueCmd {
 
     constructor(options = {}) {
+        // 任务队列数组
         this.list = [];
+
+        // 默认设置
         this.options = options;
+
+        // 是否需要开启错误提示
         this.showError = Boolean(options.showError);
     }
 
-    put(cmd = "", options = {}) {
+    initQueueObj(cmd, options) {
+        if (typeof options != "object") {
+            options = {};
+        }
         let queueObj = options;
         queueObj.cmd = cmd;
+        queueObj.state = "pending"; //pending:等待开始，running:已开始，等待结束，resolved:执行成功，rejected:执行失败
+        queueObj.cwd = options.cwd || this.options.cwd || ""
+        return queueObj;
+    }
+
+    insert(cmd = "", options = {}) {
+        let queueObj = this.initQueueObj(cmd, options);
+        if (this.list.length > 0 && this.list[0].state == "running") {
+            this.list.splice(1, 0, queueObj);
+        } else {
+            this.list.unshift(queueObj);
+        }
+    }
+
+    put(cmd = "", options = {}) {
+        let queueObj = this.initQueueObj(cmd, options);
         this.list.push(queueObj);
     }
 
-    exec(queueObj, showLog = true) {
+    exec(queueObj) {
+        console.log("\n");
+        if (typeof queueObj.cmd == "string") {
+            return this.execCmd(queueObj);
+        } else if (typeof queueObj.cmd == "function") {
+            return this.execFn(queueObj);
+        }
+    }
+
+    execCmd(queueObj, showLog = true) {
         let p = new Promise((resolve, reject) => {
-            showLog && console.log("\n");
-            showLog && console.chunk(queueObj);
-            process.exec(queueObj["cmd"], this.options, (error, stdout, stderr) => {
+
+            showLog && console.chunk(queueObj["title"] || queueObj["cmd"]);
+
+            let opt = {
+                cwd: queueObj["cwd"] || this.options["cwd"] || ""
+            };
+
+            process.exec(queueObj["cmd"], opt, (error, stdout, stderr) => {
                 let _arguments = {
                     error,
                     stdout,
                     stderr,
                 };
                 if (error !== null) {
-                    showLog && console.red(stderr);
-                    showLog && this.showError && console.red(error);
+                    showLog && console.yellow(stderr);
+                    showLog && console.red(stdout);
+                    showLog && this.showError && console.log(_arguments);
                     if (typeof queueObj.fail == 'function') {
-                        queueObj.fail({
-                            error,
-                            stdout,
-                            stderr,
-                        }, resolve, reject);
+                        queueObj.fail(_arguments, resolve, reject);
                     } else {
                         reject(_arguments);
                     }
                 } else {
-                    showLog && console.red(stderr);
+                    showLog && console.yellow(stderr);
                     showLog && console.green(stdout);
                     if (typeof queueObj.success == 'function') {
                         queueObj.success(_arguments, resolve, reject);
@@ -92,12 +133,38 @@ class QueueCmd {
         return p;
     }
 
+    execFn(queueObj) {
+        let p = new Promise((resolve, reject) => {
+            console.chunk(queueObj["title"] || "执行队列Fn");
+            queueObj.cmd.call(this, resolve, reject);
+        });
+        return p;
+    }
+
     process() {
         return new Promise(async (resolve, reject) => {
             try {
-                for (var i = 0; i < this.list.length; i++) {
-                    await this.exec(this.list[i]);
+                while (this.list.length > 0) {
+                    let queueObj = this.list[0];
+                    if (queueObj.state == "pending") {
+                        queueObj.state = "running";
+                        await this.exec(queueObj)
+                            .then((res) => {
+                                queueObj.state = "resolved";
+                                console.log(this.list);
+                                this.list.shift(0, 1);
+                            })
+                            .catch((err) => {
+                                queueObj.state = "rejected";
+                                this.list.shift(0, 1);
+                                throw err; // 注：抛出异常用以中断掉进程，若不加，会导致try后的catch不触发
+                            });
+                    } else if (queueObj.state == "resolved" || queueObj.state == "rejected") {
+                        this.list.shift(0, 1);
+                    }
                 }
+
+                console.log("\n");
                 resolve("process queue success !!!");
             } catch (err) {
                 reject(err);
@@ -108,6 +175,14 @@ class QueueCmd {
     run() {
         console.log(this.list)
         return this.process();
+    }
+
+    // 创建新的实例，共享list队列
+    extend(options = {}) {
+        let obj = new this.constructor(options);
+        //共享同一个队列
+        obj.list = this.list;
+        return obj;
     }
 }
 
@@ -125,7 +200,11 @@ class QueueGit extends QueueCmd {
                 cmd: "git symbolic-ref --short -q HEAD"
             };
             try {
-                let { error, stdout, stderr } = result = await this.exec(queueObj, false);
+                let {
+                    error,
+                    stdout,
+                    stderr
+                } = result = await this.execCmd(queueObj, false);
                 name = stdout.trim();
                 console.green(name);
                 console.green(result);
@@ -135,6 +214,31 @@ class QueueGit extends QueueCmd {
                 reject(err);
             }
         });
+    }
+
+    // 从远程ucloud创建新分支a，并推送到远程a
+    checkoutNew_from_ucloud(branchA) {
+        this.put(`git fetch`);
+        this.put(`git checkout -b ${branchA} origin/ucloud`);
+        this.put(`git push origin ${branchA}`);
+        this.put(`git branch --set-upstream-to=origin/${branchA}`);
+    }
+
+    // 提交本地分支a到远程a
+    commit_localA_push_to_originA(branchA, commitMessage) {
+        this.put(`git checkout ${branchA}`);
+        this.put("git add .");
+        this.put(`git commit -am ${commitMessage}`);
+        this.put(`git pull origin ${branchA}`);
+        this.put(`git push origin ${branchA}`);
+    }
+
+    // 切换到分支b，合并远程分支a，提交b到远程
+    push_originA_to_originB(branchA, branchB) {
+        this.put(`git checkout ${branchB}`);
+        this.put(`git pull origin ${branchB}`);
+        this.put(`git pull origin ${branchA}`);
+        this.put(`git push origin ${branchB}`);
     }
 
 }
